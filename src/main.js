8cca7f8e933a54b092711c3adbd9188179d83ee4
Cicada <202586563@qq.com>
2024-08-17 13:43:46 +0800

    ✨ feat: 使用路由koa-router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
 const Koa = require('koa')
+const userRouter = require('./router/user.route');
 const { APP_PORT } = require('./config/config.default')
 
 const app = new Koa()
 
-app.use((ctx, next) => {
-  ctx.body = 'hello world'
-})
+app.use(userRouter.routes())
 
 app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`)
