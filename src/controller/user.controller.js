7b8934a459fc91c2765b57515a5c225cef2b2e83
Cicada <202586563@qq.com>
2024-08-17 14:18:47 +0800

    ✨ feat: 添加koa-body 拆分service层

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,7 +1,14 @@
+const { createUser } = require('../service/user.service')
+
 class UserController {
   // 用户注册
   async register(ctx, next) {
-    ctx.body = 'hello users'
+    // 1.获取数据
+    const { user_name, password } = ctx.request.body
+    // 2.操作服务器
+    const res = await createUser(user_name, password)
+    // 3.返回数据
+    ctx.body = res.request.body
   }
 
   // 用户登录
