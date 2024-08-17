12296cdf5fd54418689bf46a5c6bab517a5daaf0
Cicada <202586563@qq.com>
2024-08-17 16:07:33 +0800

    ✨ feat:  统一错误处理 and 错误处理函数

--- src/middleware/user.middleware.js
+++ src/middleware/user.middleware.js
@@ -1,4 +1,5 @@
 const { getUerInfo } = require('../service/user.service')
+const { userFormateError, userAlreadyExited } = require('../constant/err.type')
 
 // 合法性
 const userValidator = async (ctx, next) => {
@@ -6,12 +7,7 @@ const userValidator = async (ctx, next) => {
 
   if (!user_name || !password) {
     console.error('用户名或密码为空', ctx.request.body)
-    ctx.status = 400
-    ctx.body = {
-      code: '10001',
-      message: '用户名或密码为空',
-      result: '',
-    }
+    ctx.app.emit('error', userFormateError, ctx)
     return
   }
 
@@ -23,12 +19,7 @@ const verifyUser = async (ctx, next) => {
   const { user_name } = ctx.request.body
 
   if (await getUerInfo({ user_name })) {
-    ctx.status = 409
-    ctx.body = {
-      code: '10002',
-      message: '用户已经存在',
-      result: '',
-    }
+    ctx.app.emit('error', userAlreadyExited, ctx)
     return
   }
 
