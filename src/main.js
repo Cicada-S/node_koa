c4f7830d47d2ce9b2a7f8692fac866f42f007a95
Cicada <202586563@qq.com>
2024-08-17 14:01:36 +0800

    ✨ feat: 将 http 服务和 app 业务拆分

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
-const Koa = require('koa')
-const userRouter = require('./router/user.route');
+const app = require('./app')
 const { APP_PORT } = require('./config/config.default')
-
-const app = new Koa()
-
-app.use(userRouter.routes())
 
 app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`)
